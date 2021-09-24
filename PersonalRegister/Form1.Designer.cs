173be9a2d8ff5360a5bb5f55f5ae14607575e51c
Sebastian <19seth@stockholmscience.se>
2021-09-24 14:25:00 +0200

    typo fix

--- PersonalRegister/Form1.Designer.cs
+++ PersonalRegister/Form1.Designer.cs
@@ -88,7 +88,7 @@ namespace PersonalRegister
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(259, 30);
             this.button1.TabIndex = 7;
-            this.button1.Text = "Registrera Försäljningen";
+            this.button1.Text = "Registrera Försäljning";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
