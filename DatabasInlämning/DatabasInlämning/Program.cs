320bf7a6bcb7eb6590887823318777afd55483ab
19seth <19seth@stockholmscience.se>
2022-05-22 20:22:26 +0200

    Prints the students and the courses they are enrolled in

--- DatabasInlämning/DatabasInlämning/Program.cs
+++ DatabasInlämning/DatabasInlämning/Program.cs
@@ -52,7 +52,7 @@ namespace DatabasInlämning
             Console.WriteLine("2) Lägg till kurs");
             Console.WriteLine("3) Lägg till läser");
             Console.WriteLine("4) Gå tillbaka");
-            Console.Write("\r\nVälj ett alternativ");
+            Console.Write("\r\nVälj ett alternativ: ");
 
             switch (Console.ReadLine())
             {
@@ -62,15 +62,48 @@ namespace DatabasInlämning
                     Console.WriteLine("--------------");
                     Console.Write("PersonNummer: ");
                     string persNr = Console.ReadLine();
-                    Console.WriteLine("Förnamn: ");
+                    Console.Write("Förnamn: ");
                     string förnamn = Console.ReadLine();
-                    Console.WriteLine("Efternamn: ");
+                    Console.Write("Efternamn: ");
                     string efternamn = Console.ReadLine();
                     Elev elev = new Elev();
                     elev.Förnamn = förnamn;
                     elev.Efternamn = efternamn;
                     elev.PersonNr = persNr;
                     service.LäggTillElev(elev);
+                    break;
+
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("Lägg till kurs");
+                    Console.WriteLine("--------------");
+                    Console.WriteLine("Kursnamnet: ");
+                    string kursNamn = Console.ReadLine();
+                    Console.WriteLine("Kurskoden: ");
+                    string kursKod = Console.ReadLine();
+                    Kurs kurs = new Kurs();
+                    kurs.KursNamn = kursNamn;
+                    kurs.KursKod = kursKod;
+                    service.LäggTillKurs(kurs);
+                    break;
+
+                case "3":
+                    Console.Clear();
+                    Console.WriteLine("Lägg till läser");
+                    Console.WriteLine("---------------");
+
+                    service.AllaElevPoster();
+                    Console.Write("\r\nVälj elev: ");
+                    int elevId = int.Parse(Console.ReadLine());
+
+                    service.AllaKursPoster();
+                    Console.Write("\r\nVälj kurs: ");
+                    int kursId = int.Parse(Console.ReadLine());
+
+                    Läser läser = new Läser();
+                    läser.ElevID = elevId;
+                    läser.KursID = kursId;
+                    service.LäggTillLäser(läser);
                     break;
                 default:
                     break;
@@ -80,18 +113,74 @@ namespace DatabasInlämning
 
         private static void TaBortMenyn()
         {
+            Console.Clear();
+            Console.WriteLine("Lägg till post");
+            Console.WriteLine("--------------");
+            Console.WriteLine("1) Ta bort elev");
+            Console.WriteLine("2) Ta bort kurs");
+            Console.WriteLine("3) Ta bort läser");
+            Console.WriteLine("4) Gå tillbaka");
+            Console.Write("\r\nVälj ett alternativ: ");
 
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    Console.Clear();
+                    Console.WriteLine("Ta bort post");
+                    Console.WriteLine("------------");
+                    service.AllaLäserPoster();
+                    Console.Write("\r\nVälj post: ");
+                    int id = int.Parse(Console.ReadLine());
+                    service.TaBortLäser(id);
+                    break;
+                default:
+                    break;
+            }
         }
 
         private static void VisaPostMenyn()
         {
+            Console.Clear();
+            Console.WriteLine("Visa poster");
+            Console.WriteLine("--------------");
+            Console.WriteLine("1) Visa alla elever");
+            Console.WriteLine("2) Visa alla kurser");
+            Console.WriteLine("3) Visa alla läser");
+            Console.WriteLine("4) Visa kurser per elev");
+            Console.WriteLine("5) Gå tillbaka");
+            Console.Write("\r\nVälj ett alternativ: ");
 
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    service.AllaElevPoster();
+                    Console.ReadLine();
+                    break;
+                case "2":
+                    service.AllaKursPoster();
+                    Console.ReadLine();
+                    break;
+                case "3":
+                    service.AllaLäserPoster();
+                    Console.ReadLine();
+                    break;
+                case "4":
+                    Console.Clear();
+                    Console.WriteLine("Visa kurs per elev");
+                    Console.WriteLine("------------------");
+                    service.AllaElevPoster();
+                    Console.Write("\r\nVälj elev: ");
+                    int id = int.Parse(Console.ReadLine());
+                    service.KurserPerElev(id);
+                    Console.ReadLine();
+                    break;
+                default:
+                    break;
+            }
         }
-
-        /*Console.WriteLine("Lägg till elev");
-            
-        
-
-        */
     }
 }
--- DatabasInlämning/DatabasInlämning/SkolService.cs
+++ DatabasInlämning/DatabasInlämning/SkolService.cs
@@ -23,5 +23,98 @@ namespace DatabasInlämning
 
             return elev;
         }
+
+        public Kurs LäggTillKurs(Kurs kurs)
+        {
+            Kontext.tblKurser.Add(kurs);
+            Kontext.SaveChanges();
+
+            return kurs;
+        }
+
+        public Läser LäggTillLäser(Läser läser)
+        {
+            Kontext.tblLäser.Add(läser);
+            Kontext.SaveChanges();
+
+            return läser;
+        }
+
+        public void findelev(int id)
+        {
+            var elev = Kontext.tblElever.Find(id);
+            Console.WriteLine(elev.Förnamn);
+            return;
+        }
+
+        public void findkurs(int id)
+        {
+            var kurs = Kontext.tblKurser.Find(id);
+            Console.WriteLine(kurs.KursNamn + " " + kurs.KursKod);
+            return;
+        }
+
+        public void AllaElevPoster()
+        {
+            foreach(var elev in Kontext.tblElever)
+            {
+                Console.WriteLine(elev.Id + " " + elev.PersonNr + " " + elev.Förnamn + " " + elev.Efternamn);
+            }
+            return;
+        }
+
+        public void AllaKursPoster()
+        {
+            foreach(var kurs in Kontext.tblKurser)
+            {
+                Console.WriteLine(kurs.KursID + " " + kurs.KursNamn + " " + kurs.KursKod);
+            }
+            return;
+        }
+
+        public void AllaLäserPoster()
+        {
+            foreach(var läser in Kontext.tblLäser)
+            {
+                Console.WriteLine(läser.ID + " " + läser.ElevID + " " + läser.KursID);
+            }
+            return;
+        }
+
+        public void TaBortLäser(int id)
+        {
+            var x = (from y in Kontext.tblLäser
+                     where y.ID == id
+                     select y).FirstOrDefault();
+            if(x != null)
+            {
+                Kontext.tblLäser.Remove(x);
+                Kontext.SaveChanges();
+            }
+            return;
+        }
+
+        public void KurserPerElev(int id)
+        {
+            var x = (from läser in Kontext.tblLäser
+                     join elev in Kontext.tblElever on läser.ElevID equals elev.Id
+                     join kurs in Kontext.tblKurser on läser.KursID equals kurs.KursID
+                     select new
+                     {
+                         elevNamn = elev.Förnamn,
+                         läserKod = läser.KursID,
+                         kursNamn = kurs.KursKod
+                     });
+            foreach(var bruh in x)
+            {
+                Console.WriteLine(bruh.läserKod + " " + bruh.elevNamn + " " + bruh.kursNamn);
+            }
+            return;
+        }
+
+        public void EleverPerKurs(int id)
+        {
+            return;
+        }
     }
 }
